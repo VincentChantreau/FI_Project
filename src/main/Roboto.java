34d7df1717df6a526dc544e3ae1037bf383f53fd
Azurelan <mbecumetailler@gmail.com>
2018-10-02 01:31:51 +0200

    project_robotoAsBeenCreated
    Peut-être que ces 26 lignes de codes formeront plus tard l'élite de l'IA..
    Ou pas, ajout d'un dictionnaire d'actions simples.

--- src/main/Roboto.java
+++ src/main/Roboto.java
@@ -1,5 +1,26 @@
 package main;
 
 public class Roboto {
+	// attributs
+	String[] picks = { "attraper", "agripper", "empoigner", "prélever", "extraire", "puiser", "appréhender", "saisir",
+			"choper", "emprunter", "ôter", "prendre" };
+	String[] uses = { "exciper", "user", "manier", "employer", "servir", "appliquer", "utiliser" };
+	String[] opens = { "frayer", "découvrir", "dégager", "entrouvrir", "déplier", "déboucher", "percer", "décapsuler",
+			"percer", "forcer", "fendre" };
 
+	// constructeur
+	public Roboto() {
+
+	}
+
+	// methodes
+	public String interprete(String s) {
+		String res = "";
+
+		return res;
+	}
+
+	public void doThis(String s) {
+		// TODO read from synonyms
+	}
 }
